87e5e260a25dbc07c02b29cdeff511dd51e9e1a7
AaronWizard <33398493+AaronWizard@users.noreply.github.com>
2021-03-11 22:41:12 -0500

    Explosion sound

--- src/game/Explosion.gd
+++ src/game/Explosion.gd
@@ -3,11 +3,15 @@ extends CPUParticles2D
 
 signal explosion_finished
 
+onready var _sound := $AudioStreamPlayer as AudioStreamPlayer
+
 
 func _ready() -> void:
 	emitting = true
 
 
 func _on_Timer_timeout() -> void:
+	if _sound.playing:
+		yield(_sound, "finished")
 	emit_signal("explosion_finished")
 	queue_free()
